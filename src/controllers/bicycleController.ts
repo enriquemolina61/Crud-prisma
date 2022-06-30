55d54089c887a138a1ef44791be0fa28b51d53f8
Enrique Molina <76474854+enriquemolina61@users.noreply.github.com>
2022-06-30 14:49:59 -0300

    validation

--- src/controllers/bicycleController.ts
+++ src/controllers/bicycleController.ts
@@ -4,7 +4,10 @@ import { CreateBicycle } from "../services/bicycleServices";
 export class BicycleController {
   async handle(request: Request, response: Response) {
     const { color, gears, brand, model, price } = request.body;
-
+    const resposta = { message: "Favor preencher todos os campos!" };
+    if (!color || !gears || !brand || !model || !price) {
+      return response.json(resposta);
+    }
     const createBicycle = new CreateBicycle();
     const result = await createBicycle.execute({
       color,
--- src/services/bicycleServices.ts
+++ src/services/bicycleServices.ts
@@ -8,13 +8,11 @@ interface ICreateBicycle {
   model: string;
   price: number;
 }
-// const result = response.json({message:"Preencha todos os campos!"});
+
 
 export class CreateBicycle {
   async execute({ color, gears, brand, model, price }: ICreateBicycle) {
-    // if (!color || !gears || !brand || !model || !price) {
-    //   return result;
-    // }
+    
     const bicycle = await prisma.bicycle.create({
       data: {
         color,
