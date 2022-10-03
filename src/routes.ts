0a4422170d5a61dd7ac3f5aaa431449abe5c6775
Enrique Molina <76474854+enriquemolina61@users.noreply.github.com>
2022-10-03 15:22:55 -0300

    rota

--- src/routes.ts
+++ src/routes.ts
@@ -4,7 +4,7 @@ import { BicycleController } from "./controllers/bicycleController";
 const routes = Router();
 const bicycle = new BicycleController();
 
-routes.post("/bicycle/", bicycle.createBicycle); //Rota para cadastrar bicicleta
+routes.post("/bicycles/create", bicycle.createBicycle); //Rota para cadastrar bicicleta
 routes.get("/bicycles/", bicycle.findAllBicycles); //Rota para filtrar bicicletar disponiveis
 routes.get("/bicycles/color/:color", bicycle.findByColor); //Rota para filtrar as bicicletas por cor
 routes.put("/bicycles/price/", bicycle.findByPrice); //Rota para filtrar bicicletas até o preço desejado
